cc22f460bf6ff2c307c227971f6b2fa51468886a
analuizaduarte <analuiisa29@gmail.com>
2024-07-01 12:24:11 -0300

    application.properties

--- src/main/java/vendas/MinhaConfiguration.java
+++ src/main/java/vendas/MinhaConfiguration.java
@@ -5,9 +5,4 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinhaConfiguration {
-
-    @Bean(name = "applicationName") //id do Bean
-    public String applicationName(){
-        return "Sistema de Vendas";
-    }
 }
